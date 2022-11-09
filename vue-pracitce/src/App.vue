51aa5a4b6c19e2ee558651d6b0e823c648c95304
shyunju7 <shyunju7@naver.com>
2022-11-09 23:39:41 +0900

    update App.vue

--- vue-pracitce/src/App.vue
+++ vue-pracitce/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoTemplate></TodoTemplate>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoTemplate from "./components/TodoTemplate.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoTemplate,
+  },
+};
 </script>
 
 <style>
